a8fa180d69fd0184e43864c8e7055ab1b563c54d
Christian Neumair <cneumair@src.gnome.org>
2002-11-28 19:36:29 +0000

    Added ChangeLog and .cvsignore.

--- misc/devhelp.el
+++
@@ -1,9 +0,0 @@
-(defun devhelp-word-at-point ()
-  "runs devhelp"
-  (interactive)
-  (setq w (current-word))
-  (start-process-shell-command "devhelp" nil "devhelp" "-s" w)
-  )
-
-; Example: bind F7 to start devhelp and search for the word at the point.
-;;(global-set-key [f7] 'devhelp-word-at-point)
