GNOME/devhelp

commit 3512bf0cc300b84a9fb626ea93b22794ab6e440c

window: convert to GtkBuilder and GActions
Converted the menu and toolbars to be GtkBuilder-based instead of GtkUIManager
based, as well as to use GActions instead of GtkActions.

The generated window menu is then set with gtk_application_set_menubar(), which
will make it be displayed properly also in fallback mode.

data/ui/devhelp.builder
@@ -50,6 +50,252 @@
     </section>
   </menu>
 
+  <menu id="window-menu">
+    <submenu>
+      <attribute name="label" translatable="yes">_Window</attribute>
+      <section>
+        <item>
+          <attribute name="label" translatable="yes">New _Tab</attribute>
+          <attribute name="action">win.new-tab</attribute>
+          <attribute name="accel">&lt;Primary&gt;t</attribute>
+          <attribute name="use-underline">True</attribute>
+        </item>
+      </section>
+      <section>
+        <item>
+          <attribute name="label" translatable="yes">_Print</attribute>
+          <attribute name="action">win.print</attribute>
+          <attribute name="accel">&lt;Primary&gt;p</attribute>
+        </item>
+      </section>
+      <section>
+        <item>
+          <attribute name="label" translatable="yes">_Close</attribute>
+          <attribute name="action">win.close</attribute>
+          <attribute name="accel">&lt;Primary&gt;w</attribute>
+        </item>
+      </section>
+    </submenu>
+    <submenu>
+      <attribute name="label" translatable="yes">_Edit</attribute>
+      <section>
+        <item>
+          <attribute name="label" translatable="yes">_Copy</attribute>
+          <attribute name="action">win.copy</attribute>
+          <attribute name="accel">&lt;Primary&gt;c</attribute>
+        </item>
+      </section>
+      <section>
+        <item>
+          <attribute name="label" translatable="yes">_Find</attribute>
+          <attribute name="action">win.find</attribute>
+          <attribute name="accel">&lt;Primary&gt;f</attribute>
+        </item>
+        <item>
+          <attribute name="label" translatable="yes">Find _Next</attribute>
+          <attribute name="action">win.find-next</attribute>
+          <attribute name="accel">&lt;Primary&gt;g</attribute>
+        </item>
+        <item>
+          <attribute name="label" translatable="yes">Find _Previous</attribute>
+          <attribute name="action">win.find-previous</attribute>
+          <attribute name="accel">&lt;Primary&gt;&lt;Alt&gt;g</attribute>
+        </item>
+      </section>
+    </submenu>
+    <submenu>
+      <attribute name="label" translatable="yes">_View</attribute>
+      <section>
+        <item>
+          <attribute name="label" translatable="yes">_Larger text</attribute>
+          <attribute name="action">win.zoom-in</attribute>
+          <attribute name="accel">&lt;Primary&gt;plus</attribute>
+        </item>
+        <item>
+          <attribute name="label" translatable="yes">S_maller text</attribute>
+          <attribute name="action">win.zoom-out</attribute>
+          <attribute name="accel">&lt;Primary&gt;minus</attribute>
+        </item>
+        <item>
+          <attribute name="label" translatable="yes">_Normal size</attribute>
+          <attribute name="action">win.zoom-default</attribute>
+          <attribute name="accel">&lt;Primary&gt;0</attribute>
+        </item>
+      </section>
+      <section>
+        <item>
+          <attribute name="label" translatable="yes">Fullscreen</attribute>
+          <attribute name="action">win.fullscreen</attribute>
+          <attribute name="accel">F11</attribute>
+        </item>
+      </section>
+    </submenu>
+    <submenu>
+      <attribute name="label" translatable="yes">_Go</attribute>
+      <section>
+        <item>
+          <attribute name="label" translatable="yes">_Back</attribute>
+          <attribute name="action">win.go-back</attribute>
+          <attribute name="accel">&lt;Alt&gt;Left</attribute>
+        </item>
+        <item>
+          <attribute name="label" translatable="yes">_Forward</attribute>
+          <attribute name="action">win.go-forward</attribute>
+          <attribute name="accel">&lt;Alt&gt;Right</attribute>
+        </item>
+      </section>
+      <section>
+        <item>
+          <attribute name="label" translatable="yes">_Search Tab</attribute>
+          <attribute name="action">win.go-search-tab</attribute>
+          <attribute name="accel">&lt;Primary&gt;s</attribute>
+        </item>
+        <item>
+          <attribute name="label" translatable="yes">_Contents Tab</attribute>
+          <attribute name="action">win.go-contents-tab</attribute>
+          <attribute name="accel">&lt;Primary&gt;c</attribute>
+        </item>
+      </section>
+    </submenu>
+  </menu>
+
+  <object class="GtkToolbar" id="toolbar">
+    <property name="visible">True</property>
+    <property name="expand">False</property>
+    <child>
+      <object class="GtkToolButton" id="back-button">
+	<property name="is-important">True</property>
+        <property name="stock_id">gtk-go-back</property>
+        <property name="action_name">win.go-back</property>
+        <property name="use_action_appearance">True</property>
+        <property name="tooltip_text" translatable="yes">Go to the previous page</property>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="homogeneous">True</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkToolButton" id="forward-button">
+	<property name="is-important">True</property>
+        <property name="stock_id">gtk-go-forward</property>
+        <property name="action_name">win.go-forward</property>
+        <property name="use_action_appearance">True</property>
+        <property name="tooltip_text" translatable="yes">Go to the next page</property>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="homogeneous">True</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkToolButton" id="zoom-out-button">
+        <property name="stock_id">gtk-zoom-out</property>
+        <property name="action_name">win.zoom-out</property>
+        <property name="use_action_appearance">True</property>
+        <property name="tooltip_text" translatable="yes">Decrease the text size</property>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="homogeneous">True</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkToolButton" id="zoom-in-button">
+        <property name="stock_id">gtk-zoom-in</property>
+        <property name="action_name">win.zoom-in</property>
+        <property name="use_action_appearance">True</property>
+        <property name="tooltip_text" translatable="yes">Increase the text size</property>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="homogeneous">True</property>
+      </packing>
+    </child>
+  </object>
+
+  <object class="GtkToolbar" id="fullscreen-toolbar">
+    <property name="visible">True</property>
+    <property name="expand">False</property>
+    <child>
+      <object class="GtkToolButton" id="fullscreen-back-button">
+	<property name="is-important">True</property>
+        <property name="stock_id">gtk-go-back</property>
+        <property name="action_name">win.go-back</property>
+        <property name="use_action_appearance">True</property>
+        <property name="tooltip_text" translatable="yes">Go to the previous page</property>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="homogeneous">True</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkToolButton" id="fullscreen-forward-button">
+	<property name="is-important">True</property>
+        <property name="stock_id">gtk-go-forward</property>
+        <property name="action_name">win.go-forward</property>
+        <property name="use_action_appearance">True</property>
+        <property name="tooltip_text" translatable="yes">Go to the next page</property>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="homogeneous">True</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkSeparatorToolItem" id="separator1" />
+      <packing>
+        <property name="expand">False</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkToolButton" id="fullscreen-zoom-out-button">
+        <property name="stock_id">gtk-zoom-out</property>
+        <property name="action_name">win.zoom-out</property>
+        <property name="use_action_appearance">True</property>
+        <property name="tooltip_text" translatable="yes">Decrease the text size</property>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="homogeneous">True</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkToolButton" id="fullscreen-zoom-in-button">
+        <property name="stock_id">gtk-zoom-in</property>
+        <property name="action_name">win.zoom-in</property>
+        <property name="use_action_appearance">True</property>
+        <property name="tooltip_text" translatable="yes">Increase the text size</property>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="homogeneous">True</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkSeparatorToolItem" id="separator2">
+        <property name="draw">False</property>
+      </object>
+      <packing>
+        <property name="expand">True</property>
+      </packing>
+    </child>
+    <child>
+      <object class="GtkToolButton" id="fullscreen-leave-fullscreen-button">
+	<property name="is-important">True</property>
+        <property name="stock_id">gtk-leave-fullscreen</property>
+	<property name="visible">True</property>
+        <property name="action_name">win.leave-fullscreen</property>
+        <property name="use_action_appearance">True</property>
+      </object>
+      <packing>
+        <property name="expand">False</property>
+        <property name="homogeneous">True</property>
+      </packing>
+    </child>
+  </object>
+
   <object class="GtkListStore" id="bookshelf_store">
     <columns>
       <!-- column-name enabled -->

data/ui/Makefile.am
@@ -1,7 +1,4 @@
 uidir = $(datadir)/devhelp/ui
-ui_DATA = 				\
-	window.ui			\
-	devhelp.builder
+ui_DATA = devhelp.builder
 
 EXTRA_DIST = $(ui_DATA)
-

data/ui/window.ui
@@ -1,52 +1,0 @@
-<!--*- xml -*-->
-<ui>
-  <menubar name="MenuBar">
-    <menu action="FileMenu">
-      <menuitem action="NewTab"/>
-      <separator/>
-      <menuitem action="Print"/>
-      <separator/>
-      <menuitem action="Close"/>
-    </menu>
-    <menu action="EditMenu">
-      <menuitem action="Copy"/>
-      <separator/>
-      <menuitem action="Find"/>
-      <menuitem action="Find Next"/>
-      <menuitem action="Find Previous"/>
-    </menu>
-    <menu action="ViewMenu">
-      <menuitem action="ZoomIn"/>
-      <menuitem action="ZoomOut"/>
-      <menuitem action="ZoomDefault"/>
-      <separator/>
-      <menuitem name="ViewFullscreenMenu" action="ViewFullscreen"/>
-    </menu>
-    <menu action="GoMenu">
-      <menuitem action="Back"/>
-      <menuitem action="Forward"/>
-      <separator/>
-      <menuitem action="ShowSearchTab"/>
-      <menuitem action="ShowContentsTab"/>
-    </menu>
-  </menubar>
-
-  <toolbar name="Toolbar">
-    <toolitem action="Back"/>
-    <toolitem action="Forward"/>
-    <separator/>
-    <toolitem action="ZoomOut"/>
-    <toolitem action="ZoomIn"/>
-  </toolbar>
-
-  <toolbar name="FullscreenToolBar">
-    <toolitem action="Back"/>
-    <toolitem action="Forward"/>
-    <separator/>
-    <toolitem action="ZoomOut"/>
-    <toolitem action="ZoomIn"/>
-    <separator expand="true"/>
-    <toolitem action="LeaveFullscreen"/>
-  </toolbar>
-
-</ui>

src/dh-app.c
@@ -309,6 +309,8 @@ setup_menu (DhApp *self)
         model = G_MENU_MODEL (gtk_builder_get_object (builder, "app-menu"));
         gtk_application_set_app_menu (GTK_APPLICATION (self), model);
 
+        model = G_MENU_MODEL (gtk_builder_get_object (builder, "window-menu"));
+        gtk_application_set_menubar (GTK_APPLICATION (self), model);
 
         g_free (path);
         g_object_unref (builder);

commit e56f96d1a5c7c57052e3bc63fc75b5afc296d7e4

fix Ctrl+C to copy
regression from 3512bf0cc300b84a9fb626ea93b22794ab6e440c

data/ui/devhelp.builder
@@ -153,7 +153,7 @@
         <item>
           <attribute name="label" translatable="yes">_Contents Tab</attribute>
           <attribute name="action">win.go-contents-tab</attribute>
-          <attribute name="accel">&lt;Primary&gt;c</attribute>
+          <attribute name="accel">&lt;Primary&gt;b</attribute>
         </item>
       </section>
     </submenu>