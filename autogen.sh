920ac0a5af0980c701fd9d9a68331d10d92f1b7b
Yosef Or Boczko <yoseforb@gmail.com>
2013-07-01 17:42:02 +0300

    Remove libgd submodule
    https://bugzilla.gnome.org/show_bug.cgi?id=701966

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,6 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="Devhelp"
-ACLOCAL_FLAGS="-I libgd $ACLOCAL_FLAGS"
 
 (test -f $srcdir/src/dh-main.c) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
