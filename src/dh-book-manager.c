cdf471ac9730ecf3e5b7e6c837505d9236884d83
Sébastien Wilmet <swilmet@gnome.org>
2015-05-23 20:31:12 +0200

    add fixme comment

--- src/dh-book-manager.c
+++ src/dh-book-manager.c
@@ -554,6 +554,7 @@ book_manager_book_deleted_cb (DhBook   *book,
         li = g_list_find (priv->books, book);
         if (li) {
                 /* Decrement language count */
+                /* FIXME */
                 book_manager_inc_language (book_manager,
                                            dh_book_get_language (book));
 
