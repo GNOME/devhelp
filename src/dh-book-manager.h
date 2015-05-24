9e9d5cab730ce7724d420eb5065f371e15c35da5
Sébastien Wilmet <swilmet@gnome.org>
2015-05-24 14:34:43 +0200

    Deprecate dh_book_manager_get_languages()
    It is used nowhere in Devhelp, and neither in Anjuta or gnome-builder.
    Removing the function would be an API break, so first deprecate it.
    For the next API break, it'll be possible to simplify the code.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=749797

--- src/dh-book-manager.h
+++ src/dh-book-manager.h
@@ -45,6 +45,8 @@ GType          dh_book_manager_get_type              (void) G_GNUC_CONST;
 DhBookManager *dh_book_manager_new                   (void);
 void           dh_book_manager_populate              (DhBookManager *book_manager);
 GList         *dh_book_manager_get_books             (DhBookManager *book_manager);
+
+G_DEPRECATED
 GList         *dh_book_manager_get_languages         (DhBookManager *book_manager);
 
 gboolean       dh_book_manager_get_group_by_language (DhBookManager *book_manager);
