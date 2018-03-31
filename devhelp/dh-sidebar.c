6e0a78bce8be3e9b2a9e4454b9b11f80531a6f91
Sébastien Wilmet <swilmet@gnome.org>
2018-03-31 14:14:03 +0200

    Update the license from GPLv2+ to GPLv3+

--- devhelp/dh-sidebar.c
+++ devhelp/dh-sidebar.c
@@ -1,5 +1,7 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*- */
 /*
+ * This file is part of Devhelp.
+ *
  * Copyright (C) 2001-2003 CodeFactory AB
  * Copyright (C) 2001-2003 Mikael Hallendal <micke@imendio.com>
  * Copyright (C) 2005-2008 Imendio AB
@@ -7,18 +9,18 @@
  * Copyright (C) 2013 Aleksander Morgado <aleksander@gnu.org>
  * Copyright (C) 2015, 2017, 2018 Sébastien Wilmet <swilmet@gnome.org>
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
+ * Devhelp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Devhelp is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ * along with Devhelp.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "dh-sidebar.h"
--- plugins/gedit-plugin/devhelp.py
+++ plugins/gedit-plugin/devhelp.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 py-indent-offset: 4 -*-
 #
 #    Gedit devhelp plugin
+#
 #    Copyright (C) 2006 Imendio AB
 #    Copyright (C) 2011 Red Hat, Inc.
 #
@@ -9,7 +10,7 @@
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
+#    the Free Software Foundation; either version 3 of the License, or
 #    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
@@ -18,8 +19,7 @@
 #    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
-#    along with this program; if not, write to the Free Software
-#    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+#    along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 from gi.repository import GObject, Gio, Gtk, Gedit
 import os
