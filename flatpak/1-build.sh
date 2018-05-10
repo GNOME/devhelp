5f26ff8f6e835aa4f191b8b21d4c0dbffec80752
Sébastien Wilmet <swilmet@gnome.org>
2018-05-10 15:59:10 +0200

    flatpak: convert manifest to YAML
    https://blogs.gnome.org/alexl/2018/05/04/yamling-the-flathub/

--- flatpak/1-build.sh
+++ flatpak/1-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 rm -rf devhelp/ repo/
-flatpak-builder devhelp org.gnome.Devhelp.json || exit 1
+flatpak-builder devhelp org.gnome.Devhelp.yml || exit 1
 flatpak build-export repo devhelp
