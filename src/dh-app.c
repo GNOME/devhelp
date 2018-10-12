c1121688fdd165d26666b8d77a2b9b04bbaa3d88
Sébastien Wilmet <swilmet@gnome.org>
2018-10-12 14:46:22 +0200

    Retire app menu: tweak other aspect of the menu
    Still part of the GNOME goal:
    https://gitlab.gnome.org/GNOME/Initiatives/wikis/App-Menu-Retirement
    
    “Rename their about menu item from "About" to "About application-name".”

--- src/dh-app.c
+++ src/dh-app.c
@@ -53,7 +53,7 @@ add_menu_action_infos (DhApp *app)
                 { "app.preferences", NULL, N_("_Preferences") },
                 { "win.shortcuts-window", NULL, N_("_Keyboard Shortcuts") },
                 { "app.help", NULL, N_("_Help"), "F1" },
-                { "app.about", NULL, N_("_About") },
+                { "app.about", NULL, N_("_About Devhelp") },
                 { "app.quit", NULL, N_("_Quit"), "<Control>q",
                   N_("Close all windows") },
 
