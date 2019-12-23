5c8a441252976db03e191240df6d0e7d620d8b5e
Ayman Bagabas <ayman.bagabas@gmail.com>
2019-12-22 20:26:41 -0500

    Licenses: Make devhelp/ files REUSE compliant
    Follow the REUSE specifications to apply licenses https://reuse.software/
    
    https://gitlab.gnome.org/GNOME/devhelp/issues/15

--- devhelp/dh-application-window.c
+++ devhelp/dh-application-window.c
@@ -1,21 +1,7 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*- */
 /*
- * This file is part of Devhelp.
- *
- * Copyright (C) 2018 Sébastien Wilmet <swilmet@gnome.org>
- *
- * Devhelp is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published
- * by the Free Software Foundation, either version 3 of the License,
- * or (at your option) any later version.
- *
- * Devhelp is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Devhelp.  If not, see <http://www.gnu.org/licenses/>.
+ * SPDX-FileCopyrightText: 2018 Sébastien Wilmet <swilmet@gnome.org>
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 #include "dh-application-window.h"
--- devhelp/dh-book-manager.h
+++ devhelp/dh-book-manager.h
@@ -1,22 +1,8 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*- */
 /*
- * This file is part of Devhelp.
- *
- * Copyright (C) 2010 Lanedo GmbH
- * Copyright (C) 2017, 2018 Sébastien Wilmet <swilmet@gnome.org>
- *
- * Devhelp is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published
- * by the Free Software Foundation, either version 3 of the License,
- * or (at your option) any later version.
- *
- * Devhelp is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Devhelp.  If not, see <http://www.gnu.org/licenses/>.
+ * SPDX-FileCopyrightText: 2010 Lanedo GmbH
+ * SPDX-FileCopyrightText: 2017, 2018 Sébastien Wilmet <swilmet@gnome.org>
+ * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 #ifndef DH_BOOK_MANAGER_H
